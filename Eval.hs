6b1f91d08b5444c1a4eb3f9565740339b8da7186
Kazato Sugimoto <kazato.sugimoto@gmail.com>
2011-12-08 22:24:08 +0900

    ifを実装

--- Eval.hs
+++ Eval.hs
@@ -7,16 +7,29 @@ primitives :: [(String, [Val] -> Val)]
 primitives = [("+", numberBinop (+)),
               ("*", numberBinop (*)),
               ("/", numberBinop div),
-              ("-", numberBinop (-))]
+              ("-", numberBinop (-)),
+              ("=", boolBinop (==)),
+              ("<", boolBinop (<)),
+              ("<=", boolBinop (<=)),
+              (">", boolBinop (>)),
+              (">=", boolBinop (>=))
+              ]
+              
+
+boolBinop :: (Integer -> Integer -> Bool) -> [Val] -> Val
+boolBinop p xs = Bool $ and $ zipWith p nums $ tail nums
+  where
+    nums = map (\(Number n) -> n) xs
 
 numberBinop :: (Integer -> Integer -> Integer) -> [Val] -> Val
 numberBinop op = Number . (foldl1 op) . mapNumber
 
-numberToInteger :: Val -> Integer
-numberToInteger (Number n) = n
-
 mapNumber :: [Val] -> [Integer]
 mapNumber = map (\(Number n) -> n)
+
+valToBool :: Val -> Bool
+valToBool (Bool False) = False
+valToBool _ = True
 
 apply :: String -> [Val] -> Val
 apply func args = maybe (Bool False) ($ args) $ lookup func primitives 
@@ -25,6 +38,10 @@ eval :: Val -> Val
 eval val@(Number _) = val
 eval val@(String _) = val
 eval val@(Bool _)   = val
+eval (List [(Symbol "if"), cond, thenVal, elseVal]) = eval $ if t then thenVal else elseVal
+                        where 
+                          t = valToBool $ eval cond
+
 eval (List ((Symbol x):args)) = apply x $ map eval args
 
 re :: String -> Val
